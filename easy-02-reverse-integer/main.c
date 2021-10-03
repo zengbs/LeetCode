310723c25b02ef9f888d78d6788c8f05fa1f5533
Po-Hsun Tseng <zengbs@gmail.com>
2021-10-03 19:32:34 +0800

    update

--- easy-02-reverse-integer/main.c
+++ easy-02-reverse-integer/main.c
@@ -1,14 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdint.h>
+#include<stdbool.h>
 #include<limits.h>
 
+
+//#define DEBUG
+
+bool checkInt32Overflow(int32_t a, int32_t b, int operation, int line)
+{
+  bool overflow = false;
+
+  if (operation == '+')
+  {
+     if ( ( b > 0 ) && ( a > INT32_MAX-b ) ) overflow = true;
+     if ( ( b < 0 ) && ( a < INT32_MIN-b ) ) overflow = true;
+
+  }
+  else if (operation == '*')
+  {
+     if (  b != 0 && a > INT32_MAX / b  )    overflow = true;
+     if (  b != 0 && a < INT32_MIN / b  )    overflow = true;
+
+  }
+  else
+  {
+     printf("something wrong!\n");
+     exit(0);
+  }
+
+# ifdef DEBUG
+  if (overflow) printf("a=%d, b=%d, line=%d\n", a, b, line);
+# endif
+
+  return overflow;
+}
+
+
 struct Stack{
-  unsigned long int capacity;
-  long int top;
-  int *stackPtr;
+  int32_t capacity;
+  int32_t top;
+  int32_t *stackPtr;
 };
 
-struct Stack* createStack(unsigned long int capacity, size_t sizePerItem)
+struct Stack* createStack(int32_t capacity, size_t sizePerItem)
 {
     struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
     stack->capacity = capacity;
@@ -23,7 +58,7 @@ void freeStack(struct Stack *stack)
   free(stack);
 }
 
-void push(struct Stack *stack, int item)
+void push(struct Stack *stack, int32_t item)
 {
   if (stack->top == stack->capacity - 1){
     printf("stack overflow!\n");
@@ -34,56 +69,31 @@ void push(struct Stack *stack, int item)
   }
 }
 
-//int pop(struct Stack *stack, int line)
-//{
-//  if (stack->top == - 1){
-//    printf("stack underflow at %d!\n", line);
-//    exit(0);
-//  }
-//  else{
-//    return stack->stackPtr[stack->top--];
-//  }
-//}
-
-//int peek(struct Stack *stack, long int line)
-//{
-//  if (stack->top == - 1){
-//    return '\0';
-//  }
-//  else{
-//    return stack->stackPtr[stack->top];
-//  }
-//}
 
 
-void displayStack(struct Stack *stack)
+int32_t displayStackInverse(struct Stack *stack)
 {
-  if (stack->top == - 1){
-    printf("stack is empty!\n");
-    exit(0);
-  }
-  else{
-      for(long int i=stack->top; i > -1; i--){
-        printf("%d", stack->stackPtr[i]);
-      }
-      printf("\n");
-  }
-}
-
-
-int displayStackInverse(struct Stack *stack)
-{
-  int output = 0;
-  int base   = 1;
+  int32_t output = 0;
+  int32_t base   = 1;
 
   if (stack->top == - 1){
     printf("stack is empty!\n");
     exit(0);
   }
   else{
-      for(long int i=stack->top; i > -1; i--){
+
+      for(int32_t i=stack->top; i > -1; i--){
+
+        if ( checkInt32Overflow( base, stack->stackPtr[i], '*', __LINE__ ) )        return 0;
+        if ( checkInt32Overflow( base*stack->stackPtr[i], output, '+', __LINE__ ) ) return 0;
 
         output += base*stack->stackPtr[i];
+
+#       ifdef DEBUG
+        printf("i=%d, stack->top=%d, output=%d\n", i, stack->top, output);
+#       endif
+
+        if ( i != 0 && checkInt32Overflow( base, 10, '*', __LINE__ ) )  return 0;
 
         base *= 10;
 
@@ -96,27 +106,37 @@ int displayStackInverse(struct Stack *stack)
 
 
 
-int reverse( int input )
+int32_t reverse( int32_t input )
 {
 
-  unsigned long int capacity = 0;
+  int32_t capacity = 0;
 
-  int positive = 0;
+  int32_t positive = 0;
 
-  if (input > 0){
+  if (input > INT32_MAX-1 || input < INT32_MIN){
+    return 0;
+  }
+
+//if input == INT32_MIN, input = -input will overflow at the line 127
+  else if (input == INT32_MIN){
+    return 0;
+  }
+  else if (input > 0){
     positive =  1;
   }
   else if (input < 0){
     positive = -1;
     input = -input;
   }
-  else{
+  else if (input == 0){
     positive =  0;
     return 0;
   }
 
-  int input2 = input;
-  int digit;
+  int32_t input2 = input;
+  int32_t digit;
+
+  printf("input=%d\n", input);
 
   while(input >= 1){
     digit = input % 10;
@@ -125,7 +145,9 @@ int reverse( int input )
     capacity++;
   }
 
-  struct Stack *stack = createStack(capacity, sizeof(int));
+  printf("capacity=%d\n", capacity);
+
+  struct Stack *stack = createStack(capacity, sizeof(int32_t));
 
   input = input2;
 
@@ -137,11 +159,12 @@ int reverse( int input )
   }
 
 
-  int output = displayStackInverse(stack);
+  int32_t output = displayStackInverse(stack);
 
   freeStack( stack );
 
   if (positive == -1) output = -output;
+
 
   return output;
 
@@ -150,9 +173,16 @@ int reverse( int input )
 int main()
 {
 
-  int input = -5432100;
+  int32_t input = -2147483648;
+
+# ifdef DEBUG
+  printf("INT32_MAX=%+d\n", INT32_MAX);
+  printf("input    =%+d\n", input);
+  printf("INT32_MIN=%+d\n", INT32_MIN);
+# endif
 
   printf("%d\n", reverse(input));
+
 
   return 0;
 }
