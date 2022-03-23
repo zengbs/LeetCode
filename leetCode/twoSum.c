033688e9439448ac8b808cabcaa1a48a9c7a1042
Po-Hsun Tseng <zengbs@gmail.com>
2022-03-23 23:39:20 +0800

    update

--- leetCode/twoSum.c
+++ leetCode/twoSum.c
@@ -1,5 +1,5 @@
 // TITLE: two sum
-// TAG: array hash
+// TAG  : array hash
 // LEVEL: easy
 
 // https://www.itread01.com/content/1543933144.html
@@ -7,11 +7,35 @@
 // Given an array of integers arry and an integer target,
 // return indices of the two numbers such that they add up to target.
 
+
+// ============================================================================
+// Key: * create an array-based hash table
+//      * the index/value of array is stored in a node rather than the hash table itself
+//      * put the value/index in the array into a newNode hashTable with the hashed value
+//        where the hashed value = abs(value) % arrySize
+//      * if collision, we insert a new node in the following manner:
+//        (hashTable[t]) --> (hashTable[t]->next)
+//        (hashTable[t]) --> (newNode) --> (hashTable[t]->next)
+//
+//      * the configuration of hash table is as follows:
+//
+//        |0| -> NULL
+//        |1| -> node1 -> NULL
+//        |2| -> NULL
+//        |3| -> node2 -> node3 -> NULL
+//        |4| -> NULL
+//
+// ============================================================================
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
 
+// =============================================================================
+//  value : the value to be inseted into linked list
+//  index : the index of the value in the array given by the problem
+// =============================================================================
 
 struct node {
     long value;
@@ -26,6 +50,7 @@ struct node {
 //               --> if collision, we insert a new node in the following manner:
 //                   (hashTable[t]) --> (hashTable[t]->next)
 //                   (hashTable[t]) --> (newNode) --> (hashTable[t]->next)
+//
 //
 // Input       : hashTable : chaining hash table
 //               value     : the value to be inseted into linked list
@@ -76,6 +101,7 @@ void freeHashTable(struct node** hashTable, int arrySize) {
     for(i = 0; i < arrySize; i++) {
         temp = hashTable[i];
         delete = temp;
+
         while(temp) {
             delete = temp;
             temp = temp->next;
@@ -96,6 +122,9 @@ int* twoSum(int* arry, int arrySize, int target) {
 
     // allocate memory for all head nodes in the hashTable
     for(i = 0; i < arrySize; i++) hashTable[i] = (struct node*)calloc(1, sizeof(struct node));
+
+    // initialize all `next` nodes are NULL
+    for(i = 0; i < arrySize; i++) hashTable[i]->next = NULL;
 
     // loop over the hashTable
     for(i = 0; i < arrySize; i++){
