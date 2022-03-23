e93568b075bbd7fcf5d4561dfb6aca0a42b87cb1
Po-Hsun Tseng <zengbs@gmail.com>
2022-03-23 11:46:37 +0800

    update

--- leetCode/twoSum.c
+++ leetCode/twoSum.c
@@ -1,11 +1,10 @@
-
 // TITLE: two sum
 // TAG: array hash
 // LEVEL: easy
 
 // https://www.itread01.com/content/1543933144.html
 
-// Given an array of integers nums and an integer target,
+// Given an array of integers arry and an integer target,
 // return indices of the two numbers such that they add up to target.
 
 #include <stdio.h>
@@ -14,45 +13,64 @@
 
 
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
 struct node {
-    long val;
+    long value;
     int index;
     struct node* next;
 };
 
+// =============================================================================
+// Description : hash(value) = abs(value) % arrySize
+// Description : (hashTable[t]) --> (hashTable[t]->next)
+//               (hashTable[t]) --> (newNode) --> (hashTable[t]->next)
+//
+// Input       : hashTable : chaining hash table
+//               value     : the value to be inseted into linked list
+//               index     : the index of the value in the array given by the problem
+//               arrySize  : the array size given by the problem
+//
+// Output      : none
+// =============================================================================
 
-void insert(struct node** hashTable, long val, int index, int n) {
-    int t = abs(val) % n;
-    struct node* temp = hashTable[t];
-    // head-add
-    struct node* add = (struct node*)malloc(sizeof(struct node));
-    add->val = val;
-    add->index = index;
-    add->next = temp->next;
-    temp->next = add;
+void insert(struct node** hashTable, long value, int index, int arrySize) {
+    int t = abs(value) % arrySize;
+
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->value = value;
+    newNode->index = index;
+    newNode->next = hashTable[t]->next;
+    hashTable[t]->next = newNode;
 }
 
 
-int search(struct node** hashTable, long target, int n) {
-    int index = abs(target) % n;
-    struct node* temp = hashTable[index]->next;
-    while(temp) {
-        if(temp->val == target) {
-            return temp->index;
+// =============================================================================
+// Input  : hashTable : chaining hash table
+//          target    : the element we are looking for
+//          arrySize  : array size given by the problem
+//
+// Output : the index of the target in a chain
+//          --> return -1 if the target is not found
+// =============================================================================
+
+int search(struct node** hashTable, long target, int arrySize) {
+    int index = abs(target) % arrySize;
+    struct node* indexNext = hashTable[index]->next;
+
+    while(indexNext) {
+        if(indexNext->value == target) {
+            return indexNext->index;
         }
-        temp = temp->next;
+        indexNext = indexNext->next;
     }
+
     return -1;
 }
 
 
-void freeHashTable(struct node** hashTable, int n) {
+void freeHashTable(struct node** hashTable, int arrySize) {
     int i = 0;
     struct node *temp = NULL, *delete = NULL;
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < arrySize; i++) {
         temp = hashTable[i];
         delete = temp;
         while(temp) {
@@ -65,47 +83,60 @@ void freeHashTable(struct node** hashTable, int n) {
 }
 
 
-int* twoSum(int* nums, int numsSize, int target) {
-    int i = 0, j = 0;
-    int n = numsSize, index = 0;
+int* twoSum(int* arry, int arrySize, int target) {
+    int i, j = 0;
+    int index = 0;
     int* result = NULL;
-    struct node** hashTable = (struct node**)malloc(n * sizeof(struct node*));
-    // init head node
-    for(i = 0; i < n; i++)
-        hashTable[i] = (struct node*)calloc(1, sizeof(struct node));
-    for(i = 0; i < n; i++) {
-        index = search(hashTable, target - nums[i], n);
-        if(-1 == index)
-            insert(hashTable, nums[i], i, n);
-        else {
+
+    // allocate memory for hashTable with the size of arrySize
+    struct node** hashTable = (struct node**)malloc(arrySize * sizeof(struct node*));
+
+    // allocate memory for all head nodes in the hashTable
+    for(i = 0; i < arrySize; i++) hashTable[i] = (struct node*)calloc(1, sizeof(struct node));
+
+    // loop over the hashTable
+    for(i = 0; i < arrySize; i++){
+
+        // search the index of complement of the target in the hashTable
+        index = search(hashTable, target - arry[i], arrySize);
+
+        // insert a new node following the head node if the complement is not found
+        if(-1 == index) insert(hashTable, arry[i], i, arrySize);
+
+        // return the indices of the target and of its complement
+        else{
             result = (int*)malloc(sizeof(int) * 2);
             result[0] = index;
             result[1] = i;
-            freeHashTable(hashTable, n);
+            freeHashTable(hashTable, arrySize);
             return result;
         }
     }
-    freeHashTable(hashTable, n);
+
+    freeHashTable(hashTable, arrySize);
+
     return result;
 }
 
 
+# define ARRY_SIZE 3
 
 int main()
 {
+  int arrySize = ARRY_SIZE;
+  int arry[ARRY_SIZE] = {3,2,4};
 
-  int returnSize;
-  int ASize = 3;
-  int A[3] = {3,2,4};
   int target = 6;
   int *returnArray = NULL;
 
-  returnArray = twoSum( A,  ASize, target);
+  returnArray = twoSum( arry,  arrySize, target);
 
   if (returnArray==NULL) {printf("returnArray=NULL!\n");exit(0);}
 
   printf("%d  ", returnArray[0]);
   printf("%d\n", returnArray[1]);
+
+  free(returnArray);
 
   return 0;
 }
