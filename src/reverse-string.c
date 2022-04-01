7345b8cc65b2a7dd4f5cca9355b107f92b9f05d7
Po-Hsun Tseng <zengbs@gmail.com>
2022-04-01 14:53:31 +0800

    update

---
+++ src/reverse-string.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+
+
+void swapChar(char *x, char *y)
+{
+  if (x != y)
+  {
+    *x ^= *y;
+    *y ^= *x;
+    *x ^= *y;
+  }
+}
+
+
+void reverseString( char s[] ){
+
+   int length = 0;
+
+   // get string length excluded ‘\0’
+   while( s[length] != '\0' ){   s[length];   length++;   }
+
+   // length is odd
+   if ( length & 1 )
+   {
+      for (int idx=0;idx<(length-1)/2;idx++) swapChar( s+idx, s+length-idx-1 );
+   }
+   // length is even
+   else
+   {
+      for (int idx=0;idx<length/2    ;idx++) swapChar( s+idx, s+length-idx-1 );
+   }
+}
+
+int main(){
+
+  char s[] = "1234567";
+
+  printf("%s\n", s);
+
+  reverseString(s);
+
+  printf("%s\n", s);
+
+  return 0;
+}
