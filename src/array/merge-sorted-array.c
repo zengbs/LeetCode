57bbabc62e6d0ba3c0807446412122cd7fd48ca3
Po-Hsun Tseng <zengbs@gmail.com>
2022-07-13 09:18:19 +0800

    update

--- src/array/merge-sorted-array.c
+++ src/array/merge-sorted-array.c
@@ -10,38 +10,53 @@
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n)
 {
-   int *ans = (int*)malloc( (m+n)*sizeof(int) );
+
+   int *mergeArray = (int*)malloc((m+n)*sizeof(int));
 
    int idx1 = 0;
    int idx2 = 0;
-   int idx  = 0;
+   int mergeIdx = 0;
 
-   while( idx1 < m && idx2 < n )
-      ans[idx++] = ( nums1[idx1] > nums2[idx2] ) ? nums2[idx2++] : nums1[idx1++];
+   while( idx1 < m && idx2 < n ){
+      if ( nums1[idx1] > nums2[idx2] ) mergeArray[mergeIdx++] = nums2[idx2++];
+      else                             mergeArray[mergeIdx++] = nums1[idx1++];
+   }
 
-   int *longArry = ( idx1 == m ) ? nums2 : nums1;
-   int longIdx   = ( idx1 == m ) ? idx2 : idx1 ;
+   int *remainder    = NULL;
+   int  remainderIdx = 0;
 
+   if ( idx1 < m ){
+      remainder    = nums1;
+      remainderIdx = idx1;
+   }
+   else{
+      remainder    = nums2;
+      remainderIdx = idx2;
+   }
 
-   for( ;idx<m+n;idx++ )      ans[idx] = longArry[longIdx++];
+   while( mergeIdx < m+n )  mergeArray[mergeIdx++] = remainder[remainderIdx++];
 
-   for ( int i=0; i<m+n; i++ ) printf("%d ", ans[i]);;
-   printf("\n");
+   for (int i=0;i<m+n;i++) nums1[i] = mergeArray[i];
+
 }
 
 
 int main (){
 
-   int arr1[] = { 2,3,4,5,6,6,7,7,7,7,8 };
-   int arr2[] = { 1,2,2,3,4,5,7,8 };
+   int arr1[] = { 1,2,3,0,0,0 };
+   int arr2[] = { 2,5,6 };
 
    int arr1Size = sizeof(arr1)/sizeof(arr1[0]);
    int arr2Size = sizeof(arr2)/sizeof(arr2[0]);
 
-   int m = arr1Size;
-   int n = arr2Size;
+   int m = 3;
+   int n = 3;
 
-   merge(arr1, arr1Size, m, arr2, arr2Size, n ) ;
+   merge(arr1, m, m, arr2, n, n ) ;
+
+   for( int i=0;i<m+n;i++ )  printf("%d ", arr1[i]);
+
+   printf("\n");
 
    return 0;
 }
