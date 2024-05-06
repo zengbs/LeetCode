e859f3e446126009fcb8a761ab4b5d65d1c9bd7e
Rocky Tseng <zengbs@gmail.com>
2024-05-06 15:42:33 +0800

    Update

--- blind-75/array/ProductExceptSelf.cpp
+++ blind-75/array/ProductExceptSelf.cpp
@@ -31,28 +31,27 @@ public:
 
 // time: O(N)
 // space: O(1)
-
-class Solution2 {
+class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
 
       int length = nums.size();
 
-      std::vector<int> prefix(length);
-      std::vector<int> suffix(length);
-      std::vector<int> answer(length);
+      std::vector<int> answer(length,1);
 
-      prefix[0]        = 1;
-      suffix[length-1] = 1;
 
-      for ( int i=1; i<length; i++ ){
-         prefix[i] = prefix[i-1] * nums[i-1];
-         suffix[length-i-1] = suffix[length-i] * nums[length-i];
+      // prefix
+      int current = 1;
+      for (int i=0; i<length; i++){
+        answer[i] *= current;
+        current *= nums[i];
       }
 
-
-      for (int i=0; i<length; i++){
-         answer[i] = prefix[i]*suffix[i];
+      // suffix
+      current = 1;
+      for(int i=length-1; i>=0; i--){
+         answer[i] *= current;
+         current *= nums[i];
       }
 
       return answer;
